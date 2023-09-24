13524d76018b9414af215bb25c22401217179978
vader2k <76834964+vader2k@users.noreply.github.com>
2023-09-24 16:57:40 +0100

    save

--- portfolio/src/pages/index.js
+++ portfolio/src/pages/index.js
@@ -1,8 +1,8 @@
 import Healthstarks from "./Healthstarks";
 import Hoobanks from "./Hoobanks";
 import Jericho from './Jericho';
-import Podcast from './podcast';
-import Social from './social';
+import Podcast from './Podcast';
+import Social from './Social';
 import Home from "./Home";
 
 export{
