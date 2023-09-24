cca78b6a8062879f04a62cb2190507420efabe66
vader2k <76834964+vader2k@users.noreply.github.com>
2023-09-24 16:03:51 +0100

    done with healthstarks section

--- portfolio/src/pages/Healthstarks.jsx
+++ portfolio/src/pages/Healthstarks.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import { Link } from 'react-router-dom'
+import back from '../assets/back.svg'
+import health from '../assets/health.png'
+import {BsArrowUpRight} from 'react-icons/bs'
 const Healthstarks = () => {
   return (
-    <div>Healthstarks</div>
+    <div className='py-10 px-3 h-[100vh]'>
+      {<Link to='/'>
+        <button><img src={back} className='w-[50px]'/></button>
+      </Link>}
+      <h1>
+        HEALTHSTARKS
+      </h1>
+      <p className='py-5 text-[13px]'>
+        Healthstarks is dedicated to fostering knowledge, facilitating collaboration, and promoting wellness within a vibrant and supportive health-conscious community. Our platform empowers individuals to ask questions, seek answers, and develop a thriving network of like-minded individuals and experts, all in the pursuit of better health and well-being.
+      </p>
+      <div className='mt-3'>
+        <img src={health}/>
+      </div>
+      <div className='mt-32 border-t border-golden py-3 w-[90%] m-auto'>
+        <div className='flex flex-col gap-5'>
+          <p>Frontend Development</p>
+          <button className='flex items-center gap-5'>see case <BsArrowUpRight/> </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- portfolio/src/pages/Hoobanks.jsx
+++ portfolio/src/pages/Hoobanks.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import back from '../assets/back.svg'
+import bank from '../assets/hoobank.png'
+import {BsArrowUpRight} from 'react-icons/bs'
 
 const Hoobanks = () => {
   return (
