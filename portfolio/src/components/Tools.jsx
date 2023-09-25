16358e09a92ca3be9b69eee5c08e92cdaf892655
vader2k <76834964+vader2k@users.noreply.github.com>
2023-09-25 18:23:59 +0100

    looks perfect

--- portfolio/src/components/Tools.jsx
+++ portfolio/src/components/Tools.jsx
@@ -10,7 +10,7 @@ const Tools = () => {
   return (
     <div className='py-14 px-3'>
       <div className=' h-[100px] w-[380px] overflow-x-auto'>
-        <div className='flex gap-5 w-[1200px] overflow-x-auto hover:text-golden'>
+        <div className='flex gap-5 w-[1000px] overflow-x-auto hover:text-golden'>
         <DiJavascript className='text-[4rem]'/> <AiFillGithub className='text-[4rem]'/> <AiFillHtml5 className='text-[4rem]'/> <BiLogoCss3 className='text-[4rem]'/> <BiLogoJquery className='text-[4rem]'/> <BiLogoReact className='text-[4rem]'/> <BiLogoBootstrap className='text-[4rem]'/> <BiLogoWordpress className='text-[4rem]'/> <BiLogoBootstrap className='text-[4rem]'/> <BiLogoTailwindCss className='text-[4rem]'/>
         <RiSupabaseFill className='text-[4rem]'/><PiFramerLogoFill className='text-[4rem]'/>
         </div>
