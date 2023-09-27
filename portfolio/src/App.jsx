1a0095ba96371ba84cda2ac8ef033470c4442e13
vader2k <76834964+vader2k@users.noreply.github.com>
2023-09-27 19:56:18 +0100

    added loading screen

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,19 +1,37 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import {Healthstarks, Hoobanks, Jericho, Podcast, Social} from './pages'
-import {Home} from './pages'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import {Healthstarks, Hoobanks, Jericho, Podcast, Social, Home} from './pages'
+import Loading from './pages/Loading'
+import React,{useState, useEffect} from 'react'
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(()=> {
+    setTimeout(()=>{
+      setLoading(false);
+    }, 4000)
+  },[]);
   return (
     <>
       <BrowserRouter>
-        <Routes>
-          <Route index element={<Home/>} />
-          <Route path='health' element={<Healthstarks/>}/>
-          <Route path='hoobanks' element={<Hoobanks/>}/>
-          <Route path='jericho' element={<Jericho/>}/>
-          <Route path='podcast' element={<Podcast/>}/>
-          <Route path='social' element={<Social/>}/>
-        </Routes>
+          <div>
+            {loading? (
+              <Loading/>
+            ):(
+              <>
+              <Routes>
+                <Route index element={<Home/>} />
+                <Route path='health' element={<Healthstarks/>}/>
+                <Route path='hoobanks' element={<Hoobanks/>}/>
+                <Route path='jericho' element={<Jericho/>}/>
+                <Route path='podcast' element={<Podcast/>}/>
+                <Route path='social' element={<Social/>}/>
+              </Routes>
+              </>
+            )
+
+            }
+          </div>
       </BrowserRouter>
 
     </>
---
+++ portfolio/src/pages/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='bg-[#131313]'>
+      <div className='flex items-center justify-center box-border w-full h-[100vh] px-5 loading '>
+        <p className='text-golden text-[1.5rem] italic font-Abril breaking'>Through out the heavens and the earth <br></br> i alone am the honored one</p>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
